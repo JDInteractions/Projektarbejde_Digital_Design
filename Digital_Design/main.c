ff6d0861e371d9b1db09c6284334a1d8af28016b
JD <jdm2303@icloud.com>
2021-06-04 13:23:59 +0200

    First Commit

---
+++ Digital_Design/main.c
@@ -0,0 +1,18 @@
+/*
+ * Digital_Design.c
+ *
+ * Created: 04-06-2021 13:21:47
+ * Author : Jan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
